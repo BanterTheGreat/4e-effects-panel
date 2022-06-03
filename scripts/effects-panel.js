812f033c326ad774966075ab300f170b3f8bde36
Imre Baas <imspons@gmail.com>
2022-06-03 11:55:57 +0200

    Initial Commit

---
+++ scripts/effects-panel.js
@@ -0,0 +1,65 @@
+class EffectsPanel extends Application {
+  constructor(){
+    super();
+  }
+
+  refresh = debounce(this.render, 100);
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "4e-easy-effects-panel",
+      popOut: false,
+      template: "modules/4e-easy-effects/static/effects-panel.html",
+    })
+  }
+
+  get actor() {
+    return canvas.tokens?.controlled[0]?.actor ?? game.user?.character ?? null;
+  }
+
+  async delete(effectId) {
+    if (this.actor) {
+      await this.actor.deleteEmbeddedDocuments("Item", [effectId]);
+      return this;
+    }
+    return super.delete();
+  }
+
+  getData(options) {
+    const data = {
+      ...super.getData(options),
+      actor: this.actor,
+      effects: [],
+    };
+
+    data.effects = data.actor?.data?.items?.filter(x => x.data.name.includes('[Effect]')).map(effect =>  {
+      const effectData = effect.clone({}, { keepId: true}).data;
+      effectData.displayName = effectData.name.split("[Effect]").pop();
+      return effectData;
+    });
+
+    return data;
+  }
+
+  activateListeners($html) {
+    super.activateListeners($html);
+
+    const $icons = $html.find("div[data-item-id]");
+
+    // Remove an effect on right-click
+    $icons.on("contextmenu", async (event) => {
+      const $target = $(event.currentTarget);
+      if ($target.attr("data-locked")) return;
+
+      const actor = this.actor;
+      const effect = actor?.items.get($target.attr("data-item-id") ?? "");
+      if (!!effect) {
+          await this.delete(effect.id);
+      } else {
+        this.refresh();
+      }
+    });
+  }
+}
+
+CONFIG.debug.hooks = false;
---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+Hooks.on("init", () => { // Move the Panel to Ui-Middle
+  game.effectsPanel = new EffectsPanel();
+
+  if (document.querySelector("#ui-top") !== null) {
+    // Template element for effects-panel
+    const uiTop = document.querySelector("#ui-top");
+    const template = document.createElement("template");
+    template.setAttribute("id", "4e-easy-effects-panel");
+    uiTop?.insertAdjacentElement("afterend", template);
+  }
+
+  game.effectsPanel.render(true);
+})
+
+Hooks.on("controlToken", () => {
+  game.effectsPanel.refresh();
+})
+
+Hooks.on("createItem", () => {
+  game.effectsPanel.refresh(); // Could be improved /w Libwrapper probably.
+})
+
+Hooks.on("deleteItem", () => {
+  game.effectsPanel.refresh();
+})
+
+// Allow Items to be dropped on Tokens in the Canvas.
+Hooks.on("dropCanvasData", (canvas, data) => {
+  const dropTarget = canvas.tokens.placeables.find((token) => {
+    const maximumX = token.x + (token.hitArea?.right ?? 0);
+    const maximumY = token.y + (token.hitArea?.bottom ?? 0);
+    return data.x >= token.x && data.y >= token.y && data.x <= maximumX && data.y <= maximumY;
+  });
+
+  const actor = dropTarget?.actor;
+
+  if (actor && data.type === "Item") {
+      if (
+        actor.sheet && ["Player Character", "NPC"].includes(actor.type)
+      ) {
+          actor.sheet._onDropItem({}, data);
+      }
+      return false; // Prevent modules from doing anything further
+  }
+  return true;
+})
